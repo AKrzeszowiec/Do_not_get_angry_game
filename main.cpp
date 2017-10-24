51dd7964b3875008789789a1ff7f652e2fb1aeb8
Alutka <alicjakrzeszowiec@gmail.com>
2017-10-24 19:21:52 +0200

    changed printing to ncurses

--- main.cpp
+++ main.cpp
@@ -14,32 +14,14 @@ public:
 
 class Tboard {
     char temporary_board[11][11];
-    Tcoord play_fields[40]={Tcoord(4,0),Tcoord(4,1),Tcoord(4,2),Tcoord(4,3),Tcoord(4,4),Tcoord(3,4),Tcoord(2,4),Tcoord(1,4),Tcoord(0,4),Tcoord(0,5),Tcoord(0,6),Tcoord(1,6),Tcoord(2,6),Tcoord(3,6),Tcoord(4,6),Tcoord(4,7),Tcoord(4,8),Tcoord(4,9),Tcoord(4,10),Tcoord(5,10),Tcoord(6,10),Tcoord(6,9),Tcoord(6,8),Tcoord(6,7),Tcoord(6,6),Tcoord(7,6),Tcoord(8,6),Tcoord(9,6),Tcoord(10,6),Tcoord(10,5),Tcoord(10,4),Tcoord(9,4),Tcoord(8,4),Tcoord(7,4),Tcoord(6,4),Tcoord(6,3),Tcoord(6,2),Tcoord(6,1),Tcoord(6,0),Tcoord(5,0)};;
+    Tcoord play_fields[40]={Tcoord(4,0),Tcoord(4,2),Tcoord(4,4),Tcoord(4,6),Tcoord(4,8),Tcoord(3,8),Tcoord(2,8),Tcoord(1,8),Tcoord(0,8),Tcoord(0,10),Tcoord(0,12),Tcoord(1,12),Tcoord(2,12),Tcoord(3,12),Tcoord(4,12),Tcoord(4,14),Tcoord(4,16),Tcoord(4,18),Tcoord(4,20),Tcoord(5,20),Tcoord(6,20),Tcoord(6,18),Tcoord(6,16),Tcoord(6,14),Tcoord(6,12),Tcoord(7,12),Tcoord(8,12),Tcoord(9,12),Tcoord(10,12),Tcoord(10,10),Tcoord(10,8),Tcoord(9,8),Tcoord(8,8),Tcoord(7,8),Tcoord(6,8),Tcoord(6,6),Tcoord(6,4),Tcoord(6,2),Tcoord(6,0),Tcoord(5,0)};;
 public:
-
-//-------------------
-    Tboard() {
-        for (int i=0; i<11; i++) {
-            for (int j=0; j<11; j++){
-                temporary_board[i][j]=' ';
-            }
-        }
-        for(int i=0; i<40; i++) {
-        temporary_board[play_fields[i].x][play_fields[i].y]='x';
-        }
-    }
-
     void print_yourself() {
-            for (int i=0; i<11; i++) {
-                for (int j=0; j<11; j++) {
-                if(j==10) {
-                cout<<temporary_board[i][j]<<endl;
-                }
-                else {
-                cout<<temporary_board[i][j]<<" ";
-                }
-                }
-            }
+    initscr();
+    for (int i=0; i<40; i++) {
+        mvaddch(play_fields[i].x,play_fields[i].y,'x');
+    }
+    keyboard_handling();
     }
 };
 
@@ -51,8 +33,8 @@ screen.print_yourself();
 }
 
 void keyboard_handling(){
-    initscr();
     noecho();
+    curs_set(0);
     keypad(stdscr, true);
     //start_color();
     bool ending=false;
